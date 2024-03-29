22d11d2ab14270c43b713506d6d5da34bf20d4c0
Markus Ebner <ebner.markus33@t-online.de>
2024-03-29 13:29:51 +0100

    Initial Commit

---
+++ Drinking Game/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Drinking Game
+//
+//  Created by Markus Private on 29.03.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Drinking Game/Drinking_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  Drinking_GameApp.swift
+//  Drinking Game
+//
+//  Created by Markus Private on 29.03.24.
+//
+
+import SwiftUI
+
+@main
+struct Drinking_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
